f7f043b0b4ab0a2459efe261dde066f7cb1586f9
siladityaa <siladityaa@gmail.com>
2016-12-11 11:03:06 +0530

    hero

--- script.js
+++ script.js
@@ -78,3 +78,15 @@ $(window).on('scroll', function() {
 		$(this).showChildren();
 	});
 });
+$(document).ready(function() {
+var movementStrength = 25;
+var height = movementStrength / $(window).height();
+var width = movementStrength / $(window).width();
+$("#hero").mousemove(function(e){
+          var pageX = e.pageX - ($(window).width() / 2);
+          var pageY = e.pageY - ($(window).height() / 2);
+          var newvalueX = width * pageX * -1 - 25;
+          var newvalueY = height * pageY * -1 - 50;
+          $('#hero').css("background-position", newvalueX+"px     "+newvalueY+"px");
+});
+});
